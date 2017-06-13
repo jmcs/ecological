a501f64b91a37e5a4b92167b8285e8454585be9d
João Santos <jmcs@jsantos.eu>
2017-06-13 18:28:22 +0200

    Update Setup Language Qualifier
    Make it clear in setup that only python 3.6 is supported

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     license='MIT License',
     classifiers=[
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
